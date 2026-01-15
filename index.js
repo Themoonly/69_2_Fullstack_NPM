dd01e25b166dc3a9cb50b2a261323dc13e6d92ed
themoonly <fewthemoonly@gmail.com>
2026-01-15 16:02:19 +0700

    initail

---
+++ index.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 7000; 
+//ถ้าหาตัวแปร PORT ใน .env ไม่เจอสามารถรันด้วย port 7000 แทนได้
+
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
